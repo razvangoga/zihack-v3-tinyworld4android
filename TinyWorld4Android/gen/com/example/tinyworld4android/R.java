b4e890bebcc2a9e397ff1c9d68e6b92efbd6fa2f
Razvan Goga <razvan.goga@gmail.com>
2013-11-08 16:55:35 +0200

    game screen layout

--- TinyWorld4Android/gen/com/example/tinyworld4android/R.java
+++ TinyWorld4Android/gen/com/example/tinyworld4android/R.java
@@ -65,10 +65,14 @@ public final class R {
         public static final int wood=0x7f020029;
     }
     public static final class id {
-        public static final int action_back=0x7f080002;
-        public static final int action_forward=0x7f080003;
-        public static final int action_settings=0x7f080001;
-        public static final int imageView1=0x7f080000;
+        public static final int action_back=0x7f080006;
+        public static final int action_forward=0x7f080007;
+        public static final int action_settings=0x7f080005;
+        public static final int buttonDown=0x7f080003;
+        public static final int buttonLeft=0x7f080001;
+        public static final int buttonRight=0x7f080002;
+        public static final int buttonUp=0x7f080000;
+        public static final int imageViewGame=0x7f080004;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -81,7 +85,12 @@ public final class R {
         public static final int action_forward=0x7f050004;
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int content_description=0x7f050009;
+        public static final int down=0x7f050006;
         public static final int game_canvas=0x7f050002;
+        public static final int left=0x7f050007;
+        public static final int right=0x7f050008;
+        public static final int up=0x7f050005;
     }
     public static final class style {
         /** 
