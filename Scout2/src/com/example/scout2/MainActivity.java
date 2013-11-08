59f1a1a10e57677e89bac9612cf266120f855877
razvan goga <razvan.goga@gmail.com>
2013-11-08 14:16:07 +0200

    hack project with resources

--- Scout2/src/com/example/scout2/MainActivity.java
+++ Scout2/src/com/example/scout2/MainActivity.java
@@ -1,8 +1,13 @@
-package com.example.tinyworld4android;
+package com.example.scout2;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.view.Menu;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -19,4 +24,14 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
+    
+    public void onLoadResourceClick(View view){
+
+    	Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.ic_launcher);
+    	ImageView iv = (ImageView)findViewById(R.id.imageView1);
+    	
+    	iv.setImageBitmap(bitmap);
+    	
+    }
+    
 }
---
+++ Scout2/src/testR/Resource.java
@@ -0,0 +1,11 @@
+package testR;
+
+import com.example.scout2.R;
+
+public class Resource {
+
+	public int test() {
+		return R.drawable.ic_launcher;
+	}
+	
+}
