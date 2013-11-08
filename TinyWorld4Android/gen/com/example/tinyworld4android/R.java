5e2aac9d2990809f81e68327e349ad5ed3eefe9b
razvan goga <razvan.goga@gmail.com>
2013-11-08 23:26:36 +0200

    handled user action

--- TinyWorld4Android/gen/com/example/tinyworld4android/R.java
+++ TinyWorld4Android/gen/com/example/tinyworld4android/R.java
@@ -66,15 +66,17 @@ public final class R {
         public static final int wood=0x7f02002a;
     }
     public static final class id {
-        public static final int action_back=0x7f080006;
-        public static final int action_forward=0x7f080007;
-        public static final int action_play=0x7f080008;
-        public static final int action_settings=0x7f080005;
-        public static final int buttonDown=0x7f080003;
+        public static final int action_back=0x7f080008;
+        public static final int action_forward=0x7f080009;
+        public static final int action_play=0x7f08000a;
+        public static final int action_settings=0x7f080007;
+        public static final int buttonClose=0x7f080005;
+        public static final int buttonDown=0x7f080004;
         public static final int buttonLeft=0x7f080001;
-        public static final int buttonRight=0x7f080002;
+        public static final int buttonRight=0x7f080003;
         public static final int buttonUp=0x7f080000;
-        public static final int imageViewGame=0x7f080004;
+        public static final int buttonUse=0x7f080002;
+        public static final int imageViewGame=0x7f080006;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -88,12 +90,14 @@ public final class R {
         public static final int action_play=0x7f050005;
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
-        public static final int content_description=0x7f05000a;
+        public static final int close=0x7f05000b;
+        public static final int content_description=0x7f05000c;
         public static final int down=0x7f050007;
         public static final int game_canvas=0x7f050002;
         public static final int left=0x7f050008;
         public static final int right=0x7f050009;
         public static final int up=0x7f050006;
+        public static final int use=0x7f05000a;
     }
     public static final class style {
         /** 
---
+++ TinyWorld4Android/src/com/example/screens/gui/UserAction.java
@@ -0,0 +1,10 @@
+package com.example.screens.gui;
+
+public enum UserAction {
+	Up,
+	Down,
+	Left,
+	Right,
+	Use,
+	Close
+}
